4e8c1cfb62e8c01a6b5733572abcea28cd6aaf0d
Dannam21 <danna.mendez@utec.edu.pe>
2024-12-03 14:49:16 -0500

    solucionando validar

--- api-tienda2/fetch_tienda.js
+++ api-tienda2/fetch_tienda.js
@@ -1,33 +1,28 @@
 const AWS = require("aws-sdk");
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
-exports.handler = async (event) => {
-    const { tenant_id, tiendaID } = event.queryStringParameters || {};
-
-    // Validar los parámetros necesarios
-    if (!tenant_id || !tiendaID) {
-        return {
-            statusCode: 400,
-            body: JSON.stringify({ message: "Faltan parámetros: tenant_id y tiendaID son necesarios" })
-        };
-    }
-
-    const params = {
-        TableName: process.env.TIENDA_TABLE,  // Usar variable de entorno para el nombre de la tabla
-        KeyConditionExpression: "tenant_id = :tenant_id AND tiendaID = :tiendaID",
-        ExpressionAttributeValues: {
-            ":tenant_id": tenant_id,
-            ":tiendaID": tiendaID,
-        },
-    };
-
+exports.handler = async () => {
     try {
-        const data = await dynamoDB.query(params).promise();
-        return { statusCode: 200, body: JSON.stringify(data.Items) };
+        const params = {
+            TableName: process.env.TIENDA_TABLE,
+        };
+
+        const result = await dynamoDB.scan(params).promise();
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: "Tiendas obtenidas exitosamente",
+                tiendas: result.Items,
+            }),
+        };
     } catch (error) {
         return {
             statusCode: 500,
-            body: JSON.stringify({ message: "Error al consultar tienda", error })
+            body: JSON.stringify({
+                message: "Error al obtener las tiendas",
+                error: error.message,
+            }),
         };
     }
 };
